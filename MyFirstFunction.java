f20341d97076218dbbdeda48ccfbff4c6e9260c4
ndayingiye <ndayiringiyedavid120@gmail.com>
2025-06-26 15:35:46 +0200

    chore:/ fuction java app setup

---
+++ MyFirstFunction.java
@@ -0,0 +1,10 @@
+public class MyFirstFunction {
+    public static void main(String[] args) {
+        sayHello();  
+        sayHello();  
+    }
+
+    public static void sayHello() {
+        System.out.println("Hello, Java!");
+    }
+}
